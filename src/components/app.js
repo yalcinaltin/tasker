6bed0a57ba089b6aae5001dfc78293204f4785a5
Cemal Yalçın Altın <cemalyalcinaltin@gmail.com>
2016-11-13 19:54:52 +0300

    init

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>React Task Reminder</h1>
+        </div>
+    );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src'
+    ],
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        filename: 'bundle.min.js'
+    },
+    resolve: {
+        modulesDirectories: ['node_modules', 'src'],
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
+    watch = true
+};
